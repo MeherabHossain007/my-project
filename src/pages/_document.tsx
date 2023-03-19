2ffc157ef5a2582da5fc22bf012e9a5b36ff220f
MeherabHossain007 <meherab788@gmail.com>
2023-03-20 01:04:12 +0600

    rifat's website

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,12 +1,17 @@
+/* eslint-disable @next/next/no-sync-scripts */
+import Header from '@/components/Header'
 import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+      <link href="https://fonts.googleapis.com/css2?family=Titillium+Web&display=swap" rel="stylesheet"/>
+      </Head>
       <body>
         <Main />
         <NextScript />
+        <script src="../path/to/flowbite/dist/flowbite.min.js"></script>
       </body>
     </Html>
   )
---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/flowbite/**/*.js",
+
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        'titillium': ['Titillium Web', 'sans']
+      }
+    },
+  },
+  plugins: [require("flowbite/plugin"),require('tailwindcss-gradient')],
+};
