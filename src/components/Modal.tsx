4969b034c0e1b36f2b2b7bceab618a527d0c3f8c
MeherabHossain007 <meherab788@gmail.com>
2023-04-03 01:48:59 +0600

    update

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,7 +1,70 @@
-import React from 'react'
+import { AddIcon } from "@chakra-ui/icons";
+import {
+  Button,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  Select,
+  Textarea,
+  useDisclosure,
+} from "@chakra-ui/react";
+import React from "react";
+import TextInput from "./TextInput";
 
-export default function Modal() {
+export default function ReqDialogue() {
+  const { isOpen, onOpen, onClose } = useDisclosure();
   return (
-    <div>Modal</div>
-  )
+    <>
+      <button
+        onClick={onOpen}
+        className="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700"
+      >
+        <svg
+          aria-hidden="true"
+          className="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
+          fill="currentColor"
+          viewBox="0 0 20 20"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path d="M5 3a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2V5a2 2 0 00-2-2H5zM5 11a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2v-2a2 2 0 00-2-2H5zM11 5a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V5zM11 13a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z"></path>
+        </svg>
+        <span className="flex-1 ml-3 whitespace-nowrap">Add Scheduled</span>
+      </button>
+      <Modal
+        closeOnOverlayClick={false}
+        isOpen={isOpen}
+        onClose={onClose}
+        size={"xl"}
+      >
+        <ModalOverlay backdropFilter="blur(10px)" />
+        <ModalContent>
+          <ModalHeader>Add Scheduled</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <TextInput onChange={() => {}} value={""} label={"Name"} />
+            <TextInput onChange={() => {}} value={""} label={"Hospital"} />
+            <TextInput onChange={() => {}} value={""} label={"Date"} />
+          </ModalBody>
+          <ModalFooter>
+            <Button mr={3} onClick={onClose} variant="ghost">
+              Close
+            </Button>
+            <Button
+              bg={"#23A6F0"}
+              textColor={"white"}
+              _hover={{ bg: "blue.400" }}
+              variant="solid"
+              onClick={(e) => {}}
+            >
+              Submit
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  );
 }
